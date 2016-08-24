8dafd27d453599f3d940d6a2f2b788b3cc6d4039
Andrew Ortego <DrewOrtegoGIS@gmail.com>
2016-08-24 11:46:45 -0700

    Describe_report
    Uploaded files for the Describe-object reporter

---
+++ Describe_Object_Report/file_list.py
@@ -0,0 +1,6 @@
+user_files = [
+    # ENTER FILE NAMES HERE!
+    u'C:\\Users\\andr7495\\Desktop\\KML.kml',
+    u'C:\\Users\\andr7495\\Desktop\\KML_point.kml\\Points',
+    u'C:\\Users\\andr7495\\Desktop\\FakeFile',
+]
